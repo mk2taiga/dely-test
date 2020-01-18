a6ead3ab799a1774849ab5d54f6f9f8c441b50d0
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-19 05:27:00 +0900

    レシピエンティティを実装

--- question1/script/domain/entity/recipe_entity.rb
+++ question1/script/domain/entity/recipe_entity.rb
@@ -1,2 +1,24 @@
+require_relative 'entity'
+
+# レシピEntity
 class RecipeEntity
+  include Entity
+
+  @recipe_id = nil
+  @material_entities = []
+  @amount_entities = []
+
+  # recipe_id: レシピを一意に識別するID
+  # material_entities: 材料の配列
+  # amount_entities: 材料の使用量の配列
+  def initialize(recipe_id, material_entities, amount_entities)
+    @recipe_id = recipe_id
+    @material_entities = material_entities
+    @amount_entities = amount_entities
+  end
+
+  # レシピのカロリーを返却する
+  def get_calorie
+  #   TODO カロリー計算処理を実装
+  end
 end
