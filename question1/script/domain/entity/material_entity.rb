247effd84eabaeb98214549dd815d51547fce44d
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-19 05:34:03 +0900

    材料エンティティを実装

--- question1/script/domain/entity/material_entity.rb
+++ question1/script/domain/entity/material_entity.rb
@@ -1,2 +1,24 @@
+require_relative 'entity'
+
+# 材料Entity
 class MaterialEntity
+  include Entity
+
+  @material_id = nil
+  @material_name = nil
+  @remark_entity = nil
+
+  # material_id: 材料を一意にに識別するID
+  # material_name: 材料名
+  # remark_entity: 備考情報
+  def initialize(material_id, material_name, remark_entity)
+    @material_id = material_id
+    @material_name = material_name
+    @remark_entity = remark_entity
+  end
+
+  # amount: 使用量
+  def get_calorie(amount)
+  # TODO 材料のカロリーを返却する処理を実装
+  end
 end
