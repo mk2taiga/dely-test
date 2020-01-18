14ce5cae68b1d271a8ada56d0e61d1a83a8ce90c
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-19 04:38:55 +0900

    ユースケース処理を実装 処理の中身はコメントにしている

--- question1/script/calculate_calorie_use_case.rb
+++ question1/script/calculate_calorie_use_case.rb
@@ -1,2 +1,16 @@
+require_relative '../script/common/const'
+
+# レシピのカロリーを計算するユースケース
 class CalculateCalorieUseCase
+  include UseCase
+
+  def execute(input)
+    if input.type == CALCULATE_CALORIE_INPUT
+      # TODO レシピEntityを取得する
+      # TODO カロリーを計算する
+      # TODO カロリーをOutPutに格納する
+      # TODO OutPutを返却する
+    end
+  end
+
 end
--- question1/script/usecase/use_case.rb
+++ question1/script/usecase/use_case.rb
@@ -1,6 +1,7 @@
 # ユースケースのモジュール
 module UseCase
   # ユースケースを実行します
+  # input: ユースケースへのインプットパラメータ
   def execute(input)
     raise NotImplementedError.new("#{self.class}##{__method__}が実装されていません")
   end
