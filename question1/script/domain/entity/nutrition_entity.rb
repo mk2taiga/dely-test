583d1370f090e51f91096d90041ae276e10d1522
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-19 05:47:12 +0900

    栄養Entityを実装

--- question1/script/domain/entity/nutrition_entity.rb
+++ question1/script/domain/entity/nutrition_entity.rb
@@ -1,9 +1,17 @@
 require_relative 'entity'
 
-# 使用量情報Entity
+# 栄養Entity
 class NutritionEntity
   include Entity
 
-  
+  @nutrition_name = nil 
+  @amount = nil
+
+  # nutrition_name: 栄養名
+  # amount: 栄養量
+  def initialize(nutrition_name, amount)
+    @nutrition_name = nutrition_name
+    @amount = amount
+  end
 
 end
