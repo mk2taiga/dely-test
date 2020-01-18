b918d9aea37675638f55a42dbe3e891a1935aab9
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-19 04:39:39 +0900

    インプットとアウトプットの処理を実装

--- question1/script/usecase/input/calculate_calorie_input.rb
+++ question1/script/usecase/input/calculate_calorie_input.rb
@@ -1,2 +1,11 @@
+require_relative '../../common/const'
+
+# カロリー計算ユースケースのインプットパラメータ
 class CalculateCalorieInput
+  include InputPort
+
+  def type
+    CALCULATE_CALORIE_INPUT
+  end
+
 end
--- question1/script/usecase/output/calculate_calorie_output.rb
+++ question1/script/usecase/output/calculate_calorie_output.rb
@@ -1,2 +1,17 @@
+# カロリー計算ユースケースのアウトプットパラメータ
 class CalculateCalorieOutput
+  include OutputPort
+
+  # カロリー
+  @calorie = nil
+
+  # calorie: カロリー
+  def initialize(calorie)
+    @calorie = calorie
+  end
+
+  def result
+    @calorie
+  end
+
 end
