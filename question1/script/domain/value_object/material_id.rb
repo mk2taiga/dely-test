987af186d09516eda2562b1659724921582f9d39
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-19 05:53:43 +0900

    ValueObjectを実装

--- question1/script/domain/value_object/material_id.rb
+++ question1/script/domain/value_object/material_id.rb
@@ -1,2 +1,14 @@
+require_relative 'value_object'
+
+# 材料を識別するIDのValueObject
 class MaterialID
+  include ValueObject
+  
+  @material_id = nil
+
+  # material_id: 材料のID
+  def initialize(material_id)
+    @material_id = material_id
+  end
+
 end
--- question1/script/domain/value_object/unit.rb
+++ question1/script/domain/value_object/unit.rb
@@ -1,2 +1,14 @@
+require_relative 'value_object'
+
+# 使用単位のValueObject
 class Unit
+  include ValueObject
+  
+  @unit_name = nil
+
+  # unit_name: 使用単位を識別する名前
+  def initialize(unit_name)
+    @unit_name = unit_name
+  end
+
 end
