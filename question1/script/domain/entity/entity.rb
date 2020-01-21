2b44d09dabe599b4b3154029eec0e7abaebab9c0
Taiga Miyazaki <mk2_tg000@icloud.com>
2020-01-21 21:30:36 +0900

    不要な処理を削除

--- question1/script/domain/entity/entity.rb
+++ question1/script/domain/entity/entity.rb
@@ -1,8 +1,4 @@
 # エンティティのモジュール
 # 識別子が同じであれば、実態も同じとみなすオブジェクト
 module Entity
-  # Entityが等しいかどうかを判定する
-  # 比較対象のオブジェクト
-  def equals(object)
-  end
 end
--- question1/script/domain/value_object/value_object.rb
+++ question1/script/domain/value_object/value_object.rb
@@ -1,8 +1,4 @@
 # ValueObjectのモジュール
 # 生成後不変のオブジェクト
 module ValueObject
-  # ValueObjectが等しいかどうかを判定する
-  # 比較対象のオブジェクト
-  def equals(object)
-  end
 end
